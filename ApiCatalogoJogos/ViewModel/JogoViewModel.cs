75bf50a9614a4d7fcab8a749fffc2491cb873383
Karinne Massensini <karinneamp22@gmail.com>
2021-09-20 17:13:04 -0300

    ApiCatalogoJogos
    Projeto DecolaTech

---
+++ ApiCatalogoJogos/ViewModel/JogoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.ViewModel
+{
+    public class JogoViewModel
+    {
+        internal double Preco;
+
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+        
+        public string Produtora { get; set; }
+
+        public double Preço { get; set; }
+    
+    }
+}
